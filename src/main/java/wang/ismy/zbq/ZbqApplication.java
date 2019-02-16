91e68a33907aaf1b64098e175a4824942fe33eae
最后一个NPE <chenjiping9527@gmail.com>
2019-02-16 16:36:44 +0800

    Initial commit

---
+++ src/main/java/wang/ismy/zbq/ZbqApplication.java
@@ -0,0 +1,14 @@
+package wang.ismy.zbq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZbqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZbqApplication.class, args);
+    }
+
+}
+
